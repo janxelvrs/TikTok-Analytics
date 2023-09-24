d2e8e3b65abf961312446ea8b2f914e2b72dab81
jan.elvers <jan.elv.je@gmail.com>
2023-09-24 18:25:04 +0200

    weird change

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def trending_videos():
 async def user_videos():
     async with TikTokApi() as api:
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
-        async for video in api.user(username="wheel.world").videos():
+        async for video in api.user(username="emma.davis05").videos():
             # print(api.user(username="emma.davis05").videos(count = 1000))
             #if "contents" in video.as_dict:
             print(video.as_dict)
